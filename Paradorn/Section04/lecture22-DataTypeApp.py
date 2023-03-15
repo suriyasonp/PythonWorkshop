da1ef72346e8d3eaa8e41d251a42bd1221333024
Paradorn Sonjai <paradorn.sonjai@gmail.com>
2023-03-15 08:38:22 +0700

    New lecture22-DataTypeApp

---
+++ Paradorn/Section04/lecture22-DataTypeApp.py
@@ -0,0 +1,17 @@
+print("Welcome to Fruit Shop")
+print("---------------------------")
+
+Orange = int(30)
+grape  = int(50)
+Mango  = int(70)
+durian = int(80)
+
+print("Orange ",3 , Orange ," THB"," Total ", Orange*3," THB")
+print("grape  ",1 , grape ," THB", " Total " , grape*1," THB")
+print("Mango  ",5 , Mango ," THB", " Total " , Mango*5," THB")
+print("durian ",10 , durian ,"THB"," Total " , durian*5," THB")
+Tatal = int(Orange*3 + grape*1 +Mango*5+durian*5)
+
+print("---------------------------")
+print("Total 100THB",Tatal , " THB" )
+print("Thank You : Fruit Shop")
