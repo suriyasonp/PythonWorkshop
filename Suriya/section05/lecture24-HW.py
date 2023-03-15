2b9e3552c1b1474cc38d9c7213be01926b194ebf
Suriya S <suriyasonp.tech@gmail.com>
2023-03-15 08:13:57 +0700

    Add lecture 24 files

---
+++ Suriya/section05/lecture24-HW.py
@@ -0,0 +1,14 @@
+"""
+Create variables to store Fullname, Age, Height, Weight
+"""
+
+fullName = "Robert Tingnongnoi"
+age = 29
+height = 180
+weight = 70
+
+print("********Person Information********")
+print("Fullname: ", fullName)
+print("Age:", age)
+print("Height:", height)
+print("Weight:", weight)
