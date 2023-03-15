44f73d5a864a3fd59673d05401f70fa5bdef823b
Paradorn Sonjai <paradorn.sonjai@gmail.com>
2023-03-15 08:42:30 +0700

    Update Datatypeapp

--- Paradorn/Section04/lecture22-DataTypeApp.py
+++ Paradorn/Section04/lecture22-DataTypeApp.py
@@ -5,13 +5,17 @@ Orange = int(30)
 grape  = int(50)
 Mango  = int(70)
 durian = int(80)
+watermelon  = int(70)
 
-print("Orange ",3 , Orange ," THB"," Total ", Orange*3," THB")
-print("grape  ",1 , grape ," THB", " Total " , grape*1," THB")
-print("Mango  ",5 , Mango ," THB", " Total " , Mango*5," THB")
-print("durian ",10 , durian ,"THB"," Total " , durian*5," THB")
-Tatal = int(Orange*3 + grape*1 +Mango*5+durian*5)
+
+print("Orange ",3 ,  Orange ," THB"," Total ", Orange*3," THB")
+print("grape  ",1 ,  grape ," THB", " Total " , grape*1," THB")
+print("Mango  ",5 ,  Mango ," THB", " Total " , Mango*5," THB")
+print("durian ",10 , durian ,"THB"," Total " , durian*10," THB")
+print("durian ",4 ,  watermelon ,"THB"," Total " , watermelon*4," THB")
+
+Tatal = int(Orange*3 + grape*1 +Mango*5+durian*10+watermelon*4)
 
 print("---------------------------")
-print("Total 100THB",Tatal , " THB" )
+print("Total ",Tatal , " THB" )
 print("Thank You : Fruit Shop")
