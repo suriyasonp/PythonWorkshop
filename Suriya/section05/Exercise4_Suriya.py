5cfde2f41aae8fc6291af5cf46e52309b142f8b4
Suriya S <suriyasonp.tech@gmail.com>
2023-03-15 08:25:48 +0700

    Add lecture 24 files

---
+++ Suriya/section05/Exercise4_Suriya.py
@@ -0,0 +1,28 @@
+"""
+"หากผู้เรียนกำลังศึกษาอยู่ในชั้นปีที่ 2 มหาวิทยาลัยชื่อดังย่านกลางเมืองที่ตอนนี้กำลังสอบเก็บคะแนนกันอย่างสนุกสนาน แล้ววันนึงได้รับมอบหมายจากอาจารย์ที่ปรึกษาให้พัฒนาโปรแกรมให้กับทางคณะ โดย ระบบดังกล่าวจะมีการเก็บคะแนนรายบุคคลของผู้เรียนแต่ละท่าน ของนักศึกษาชั้นปีที่ 2 เทอม 1" โดยมีรายชื่อวิชาดังนี้
+
+Foundation English
+General Business
+Introduction to Computer Systems
+Computer Programming
+
+โดยให้ผู้เรียนพัฒนาโปรแกรมโดยสร้างตัวแปรสำหรับเก็บคะแนนผู้เรียนในรายวิชาดังกล่าว โดยคะแนนจะสามารถเป็นตัวเลขทศนิยมได้ และ เมื่อได้ตัวแปรมาแล้วให้ทำการแสดงผลออกมาให้ผู้ใช้งานได้เห็นในรูปแบบ
+
+--- Your Score ---
+Foundation English : คะแนนที่ได้
+General Business : คะแนนที่ได้
+Introduction to Computer Systems : คะแนนที่ได้
+Computer Programming : คะแนนที่ได้
+*การตั้งชื่อตัวแปรควรสื่อความหมาย ผู้อ่าน หรือ นักพัฒนาโปรแกรมท่านอื่นสามารถอ่านเข้าใจได้
+"""
+
+foundationEnglishScore = 80
+generalBusiness = 75
+introductionComSci = 95
+computerProgramming = 100
+
+print("--- Your Score ---")
+print("Foundation English : ", foundationEnglishScore)
+print("General Business :", generalBusiness)
+print("Introduction to Computer Systems : ", introductionComSci)
+print("Computer Programming :", computerProgramming)
