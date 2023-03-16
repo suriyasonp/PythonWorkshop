d2e195ab2ae9c745b177908f2f23fc5cc0d68a01
Paradorn Sonjai <paradorn.sonjai@gmail.com>
2023-03-16 10:39:53 +0700

    update lecture28

--- Paradorn/section05/lecture26-MatchOperatorExample.py
+++ Paradorn/section05/lecture26-MatchOperatorExample.py
@@ -5,5 +5,6 @@ costPerDay = 120
 result  = money + (incomePerDay * 30) - (costPerDay * 30)
 result2  = money + incomePerDay * 30 - costPerDay * 30
 
+
 print("Result 1 =",result)
 print("Result 2 =",result2)
