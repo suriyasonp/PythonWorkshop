c0abf84a339cafbb1db0d2c96ec6ccc10c9403e5
Paradorn Sonjai <paradorn.sonjai@gmail.com>
2023-03-16 08:29:29 +0700

    new section05

---
+++ Paradorn/section05/Exercise4_Paradorn_S.py
@@ -0,0 +1,13 @@
+
+FoundationEnglish = float(3.5)
+GeneralBusiness = float(2.5)
+IntroductionComputerSystems = float(1.5)
+ComputerProgramming = float(3.5)
+Topic = "--- Your Score ---"
+
+print(Topic)
+print('\n')
+print("Foundation English :",FoundationEnglish)
+print("General Business :",GeneralBusiness)
+print("Introduction to Computer Systems :",IntroductionComputerSystems)
+print("Computer Programming :",ComputerProgramming)
---
+++ Paradorn/section05/lecture24-VariableExample.py
@@ -0,0 +1,13 @@
+x = 50
+y = 66
+print(type(x))
+print(x)
+
+print(type(y))
+print(y)
+name = "Paradorn Sonjai"
+age = 35
+tall =  180
+weight = 83
+
+print("Myname is :",name , "age :",age, "Tal :" ,tall, " Weight :", weight   )
