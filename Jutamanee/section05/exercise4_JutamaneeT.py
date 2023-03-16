65db38e7687d2c1f242c1759e61ecf8cf79ddcf6
jutamanee61 <52960791+jutamanee61@users.noreply.github.com>
2023-03-16 08:33:31 +0700

    Add exercise4

---
+++ Jutamanee/section05/exercise4_JutamaneeT.py
@@ -0,0 +1,11 @@
+FoundationEnglish=95
+GeneralBusiness=75
+IntroductiontoComputerSystems=50
+ComputerProgramming=80
+
+print("--- Your Score ---")
+print("Foundation English :",FoundationEnglish)
+print("General Business :",GeneralBusiness)
+print("Introduction to Computer Systems :",IntroductiontoComputerSystems)
+print("Computer Programming :",ComputerProgramming)
+print("------------------")
