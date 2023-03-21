bc149b3972c984b20f432af7205807bffcea182d
jutamanee61 <52960791+jutamanee61@users.noreply.github.com>
2023-03-21 08:24:32 +0700

    Add lecture29AssignmentPeratorExample

---
+++ Jutamanee/section05/lecture29AssignmentPeratorExample.py
@@ -0,0 +1,24 @@
+a = 10
+b = 5
+c = a + b
+b = 10
+print(b)
+
+a = 10
+b = 12
+c = a + b
+a+= 5
+'''=> a = a + 5 '''
+print(a)
+
+a = 10
+a = a + 5
+print(a)
+
+a = a - 1
+print(a)
+
+money = 1000
+money -= 50
+money -= 20
+print("Money =",money)
