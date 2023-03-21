201e6b91fef47a1d029505d40ff295a0ecb0fa81
Suriya S <suriyasonp.tech@gmail.com>
2023-03-21 08:56:33 +0700

    add lecture 33 and exercise 5-1

---
+++ Suriya/section05/Exercise5_1_Suriya_S.py
@@ -0,0 +1,15 @@
+x = int(input("Enter integer number:"))
+y = int(input("Enter 2nd integer number:"))
+print(x, "+", y, "=", x + y)
+print(x, "-", y, "=", x - y)
+print(x, "*", y, "=", x * y)
+print(x, "/", y, "=", int(x / y))
+
+'''Result
+Enter integer number:10
+Enter 2nd integer number:2
+10 + 2 = 12
+10 - 2 = 8
+10 * 2 = 20
+10 / 2 = 5
+'''
