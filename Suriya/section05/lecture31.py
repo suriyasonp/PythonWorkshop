19f59b1bf070187a906bcf9dd98413805198cb51
Suriya S <suriyasonp.tech@gmail.com>
2023-03-21 08:46:13 +0700

    add lecture 31-32

---
+++ Suriya/section05/lecture31.py
@@ -0,0 +1,8 @@
+a = "10"
+b = 5
+"""Convert data type"""
+c = int(a) + b
+print(c)
+
+c = float(a)+b
+print(c)
---
+++ Suriya/section05/lecture32.py
@@ -0,0 +1,4 @@
+print("Please enter a number or text.")
+x = input()
+print("The input data is ", x, ".")
+print("The input data type is ", type(x))
