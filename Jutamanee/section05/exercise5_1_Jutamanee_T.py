4067643a457f4fb8ac75ea7303be78ba3917446c
jutamanee61 <52960791+jutamanee61@users.noreply.github.com>
2023-03-22 09:19:44 +0700

    Add exercise5_1_Jutamanee_T

---
+++ Jutamanee/section05/exercise5_1_Jutamanee_T.py
@@ -0,0 +1,7 @@
+number1=int(input("number1:"))
+number2=int(input("number2:"))
+
+print(number1,"+",number2,"=",number1+number2)
+print(number1,"-",number2,"=",number1-number2)
+print(number1,"*",number2,"=",number1*number2)
+print(number1,"/",number2,"=",int(number1/number2))
