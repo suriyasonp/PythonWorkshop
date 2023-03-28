65411a0250487ca5a67d45ce6f8192aaf38a5390
Suriya S <suriyasonp.tech@gmail.com>
2023-03-28 11:05:28 +0700

    Add lecture48

--- Suriya/section07/lecture47.py
+++ Suriya/section07/lecture47.py
@@ -1,9 +1,11 @@
-start = 2
 result = 0
 for i in range(12):
     for j in range(12):
-        result = start * (j + 1)
+        result = (i + 1) * (j + 1)
         print(str(i + 1) + " x " + str(j + 1) + " = " + str(result))
         if (j + 1) % 12 == 0:
             print("-----------------")
-    start += 1
+    if i % 2 == 1:
+        break
+    elif i % 2 == 0:
+        continue
