25cf48bb6a5b3548484685a13e8ee331b4acf528
jutamanee61 <52960791+jutamanee61@users.noreply.github.com>
2023-03-30 08:48:09 +0700

    Add lecture46_NestedLoopExample

---
+++ Jutamanee/section07/lecture46_NestedLoopExample.py
@@ -0,0 +1,36 @@
+'''print("2 x 1 = 2")
+print("2 x 2 = 4")
+print("2 x 3 = 6")
+print("2 x 4 = 8")
+print("2 x 5 = 10")
+print("2 x 6 = 12")
+print("2 x 7 = 14")
+print("2 x 8 = 16")
+print("2 x 9 = 18")
+print("2 x 10 = 20")
+print("2 x 11 = 22")
+print("2 x 12 = 24")'''
+'''print("/////////=1=//////////")'''
+'''x = 1
+y = 2*x
+print("2 x",x,"=",y)
+x = x+1
+y = 2*x
+print("2 x",x,"=",y)
+x = x+1
+y = 2*x
+print("2 x",x,"=",y)'''
+'''print("/////////=2=//////////")'''
+'''for x in range(12):
+    print("2 x", x+1, "=",2*(x+1))
+print("--------------------------")
+for x in range(12):
+    print("3 x", x+1, "=",3*(x+1))
+print("--------------------------")
+for x in range(12):
+    print("4 x", x+1, "=",4*(x+1))'''
+'''print("/////////=3=//////////")'''
+for x in range(12):
+    for y in range(12):
+        print(x+1,"x", y+1, "=",(x+1)*(y+1))
+
