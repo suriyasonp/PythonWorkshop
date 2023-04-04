2b6855ebd22421722fe030fdcd62afb13d625672
jutamanee61 <52960791+jutamanee61@users.noreply.github.com>
2023-04-04 09:32:53 +0700

    Add lecture50_FunctionParamExample

---
+++ Jutamanee/section08/lecture50_FunctionParamExample.py
@@ -0,0 +1,15 @@
+def plusNumber(x,y):
+    print(x+y)
+plusNumber(4,2)
+
+def minusNumber(x,y):
+    print(x-y)
+minusNumber(4,2)
+
+def multiplyNumber(x,y):
+    print(x*y)
+multiplyNumber(4,2)
+
+def divideNumber(x,y):
+    print(x/y)
+divideNumber(4,2)
