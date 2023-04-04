de281129b66b228d437ec2ed1f6ad6fec482d4fa
Paradorn Sonjai <paradorn.sonjai@gmail.com>
2023-04-04 15:28:49 +0700

    lecture54

---
+++ Paradorn/section08/lecture49_Function.py
@@ -0,0 +1,10 @@
+def sayHelloWorld():
+    print("Hello World")
+    sayHello()
+    print("Wooooooooo!!")
+def sayHello():
+    print("Hello Noon!")
+    print("Hahaa")
+
+sayHelloWorld()
+sayHello()
---
+++ Paradorn/section08/lecture52.py
@@ -0,0 +1,7 @@
+def sayHello(name):
+    return "Hello" ,name
+print(sayHello("Test"))
+
+def number():
+    return 50
+print(number())
