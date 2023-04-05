cb058c50015a5632cec7c443d591386cb3748c19
jutamanee61 <52960791+jutamanee61@users.noreply.github.com>
2023-04-05 16:36:49 +0700

    Add lecture58_StringFormatExample

---
+++ Jutamanee/section09/lecture58_StringFormatExample.py
@@ -0,0 +1,6 @@
+print("Hello "+"Fern! and weight is " +"50 " +"kg")
+name ="Fern"
+weight= "50"
+print("Hello "+name+"! and weight is " + weight +"kg")
+print("Hello %s!" "and weight is %s kg" %(name,weight))
+print("Hello %s!" "and weight is %d kg" %(name,55.10))
