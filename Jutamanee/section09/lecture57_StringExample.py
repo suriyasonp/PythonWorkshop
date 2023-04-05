9bf4858c9ba5a8a82e81e86c30624077d49605f0
jutamanee61 <52960791+jutamanee61@users.noreply.github.com>
2023-04-05 16:22:22 +0700

    Add lecture57_StringExample

---
+++ Jutamanee/section09/lecture57_StringExample.py
@@ -0,0 +1,11 @@
+text = "Hello"
+print(text[0])
+print(text[0]+"Hello")
+print(text[0]+text[1]+text[2]+text[3])
+print(text[0:4])
+
+address = "642/7 Sukhumvit Bangkok"
+print("Bangkok" in address)
+print("Sukhumvit" in address)
+print("Sukhumvit" not in address)
+'''Sukhumvit ไม่ได้อยู่ใน address จริงหรือไม่ ? '''
