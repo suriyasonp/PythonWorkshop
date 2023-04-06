af2b2b62ac8737551fcd7c379c645be14fad0726
jutamanee61 <52960791+jutamanee61@users.noreply.github.com>
2023-04-06 08:07:35 +0700

    Add lecture63_ListExample

---
+++ Jutamanee/section10/lecture63_ListExample.py
@@ -0,0 +1,10 @@
+'''Ex.1'''
+list1 = [0,2,4,6,8]
+for i in list1:
+    print(i)
+'''Ex.2'''
+customerList = ["Fern","Cat","Dog"]
+print(customerList[0])
+print(customerList[1:2])
+print(customerList[:2])
+print(customerList[:3])
