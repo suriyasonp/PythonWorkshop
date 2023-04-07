df91b9884b4335aa99c6bc883548eaff67b2bce2
jutamanee61 <52960791+jutamanee61@users.noreply.github.com>
2023-04-07 13:44:12 +0700

    Add lecture69_DictionaryExample

---
+++ Jutamanee/section10/lecture69_DictionaryExample.py
@@ -0,0 +1,10 @@
+dict1 = {'Name':'James','Age':18}
+print(dict1['Name'],dict1['Age'])
+
+'''Ex Add data to dict '''
+dict1['LastName'] = 'Kotlin'
+print("Hello:",dict1['Name'],dict1['LastName'])
+
+'''Ex Chang data to dict '''
+dict1['Name'] = 'Anna'
+print(dict1)
