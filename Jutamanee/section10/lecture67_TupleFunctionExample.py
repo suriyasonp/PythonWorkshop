7d113f7ab3857d680125895895b7783c0e5b14c5
jutamanee61 <52960791+jutamanee61@users.noreply.github.com>
2023-04-07 10:49:08 +0700

    Add lecture67_TupleFunctionExample

---
+++ Jutamanee/section10/lecture67_TupleFunctionExample.py
@@ -0,0 +1,22 @@
+tupleEx = (1,200,3,4,10)
+listEx = (1,200,3,4,10)
+
+print(listEx)
+len(tupleEx)
+
+'''Ex.1'''
+print(len(tupleEx))
+'''ฟังก์ชั่น len() ใช้แสดงจำนวนสมาชิกของ list,tuple,string,collection ต่างๆ '''
+
+
+'''Ex.2'''
+print(max(tupleEx))
+'''ฟังก์ชั่น max() แสดงข้อมูลที่มากที่สุด'''
+
+'''Ex.3'''
+print(tuple(listEx))
+'''ฟังก์ชั่น tuple() เปลี่ยนข้อมูล list เป็น tuple '''
+
+'''Ex.4'''
+print(tupleEx.index(200))
+'''ฟังก์ชั่น index() ใช้สำแสดงตำแหน่งของข้อมูล'''
