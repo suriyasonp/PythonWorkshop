5698b6351f764cc5bf2f42ba959221ffa24872e0
suriyasonp <suriya.sonphu@gmail.com>
2023-04-08 11:39:40 +0700

    continue on lecture72

--- Suriya/section10/lecture71.py
+++ Suriya/section10/lecture71.py
@@ -1,14 +1,16 @@
+# Example to use list. But this example is not the nice solution to be used.
+# Because of menu list and price are seperated and getting wrong informaton.
 def showBill():
     totalPrice = 0
     print("Smile Cooker".center(20, "-"))
     for i in range(len(menuList)):
         totalPrice += float(priceList[i])
-        print(str(i+1), " Item:" + menuList[i] + "   Price: " + str(priceList[i]))
+        print(str(i + 1), " Item:" + menuList[i] + "   Price: " + str(priceList[i]))
     print("Total Price: ", totalPrice)
+
 
 menuList = []
 priceList = []
-itemCollection = {}
 while True:
     menuName = input("Please enter menu: ").lower()
     if (menuName.lower() == "exit"):
