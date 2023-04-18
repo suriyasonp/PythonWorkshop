926b69e06d7f3ac2f7acf8de9d8607f3184009bb
Paradorn Sonjai <paradorn.sonjai@gmail.com>
2023-04-18 09:42:13 +0700

    d

---
+++ Paradorn/section08/lecture55.py
@@ -0,0 +1,6 @@
+import math
+
+print(math.ceil(10.59))
+print(math.floor(10.59))
+print(math.factorial(4))
+print(math.pow(2,2))
---
+++ Paradorn/section09/lecture58.py
@@ -0,0 +1,3 @@
+name = "Lion"
+weight = 65
+print("My name is %s and my weight is %fKg" % (name, weight))
