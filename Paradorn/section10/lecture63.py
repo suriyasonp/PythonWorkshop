068d07856663a7c3948984e3c6e969c5a85cba87
Paradorn Sonjai <paradorn.sonjai@gmail.com>
2023-04-19 08:56:02 +0700

    section09

---
+++ Paradorn/section10/lecture63.py
@@ -0,0 +1,6 @@
+list1 = [0,1,2,3,4]
+customerList = ["Leon","Peter"]
+print(customerList[0])
+print("-------------------")
+for i in customerList:
+    print(i)
---
+++ Paradorn/section10/lecture67.py
@@ -0,0 +1,19 @@
+tupleEx = (1, 2, 199, 4, 5)
+print(len(tupleEx))     # แสดงจำนวนข้อมูลใน tuple
+print(max(tupleEx))     # แสดงข้อมูลที่มากที่สุดใน tuple
+print(min(tupleEx))     # แสดงข้อมูลที่น้อยที่สุดใน tuple
+print(tupleEx.index(199))
+
+print(list(tupleEx))
+
+print("-------------------")
+
+list1 = [5, 6, 7, 8]
+print(list1)
+tuple1 = tuple(list1)
+print(tuple1)
+
+tupleEx = (1, 2, 199, 4, 5)
+print(tupleEx)
+listEx = list(tupleEx)
+print(listEx)
