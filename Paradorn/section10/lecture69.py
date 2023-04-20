13179998d305ddd1e3ca498d00965854de00f3e3
Paradorn Sonjai <paradorn.sonjai@gmail.com>
2023-04-20 08:59:19 +0700

    lecture69

--- Paradorn/section10/lecture69.py
+++ Paradorn/section10/lecture69.py
@@ -1,7 +1,8 @@
-dict1 = {"name":"Leon","Age":30}
 
-dict1["last"] = "Cenady"
-print("Hello",dict1["name"],dict1["last"])
+dict1 = {'name':'Leon','Age':30}
+
+dict1['lastName'] = 'Cenady'
+print("Hello",dict1['name'],dict1['lastName'])
 dict1.clear()
-dict1[""]
+
 print(dict1)
