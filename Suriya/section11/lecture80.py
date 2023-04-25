d6ad20da69d875c363556cfd1379ea93c35e5e93
Suriya S <suriyasonp.tech@gmail.com>
2023-04-25 08:49:25 +0700

    add lecture 79-81

--- Suriya/section11/lecture80.py
+++ Suriya/section11/lecture80.py
@@ -1,10 +1,15 @@
-try:
-    input1 = int(input("A: "))
-    input2 = int(input("B: "))
-    print(input1 / input2)
-except ValueError:
-    print("Error! Please re-enter number.")
-except ZeroDivisionError:
-    print("Error! You can't enter 0.")
-except:
-    print("Something went wrong!")
+# Try Catch
+
+import sys
+
+randomList = ['a', 0, 2]
+for entry in randomList:
+    try:
+        print("The entry is", entry)
+        r = 1 / int(entry)
+        break
+    except:
+        print("Oops!", sys.exc_info()[0], "occured.")
+        print("Next ecntry.")
+        print()
+print("The reciprocal of", entry, "is", r)
