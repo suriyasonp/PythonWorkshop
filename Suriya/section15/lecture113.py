96c77ec254ff54f304e4d82feb8630338b58fbfc
Suriya S <suriyasonp.tech@gmail.com>
2023-05-09 10:08:29 +0700

    add lecture 113

--- Suriya/section15/lecture113.py
+++ Suriya/section15/lecture113.py
@@ -1,4 +1,9 @@
 from forex_python.converter import CurrencyRates
+
 c = CurrencyRates()
 currentResult = c.get_rates('THB')
 print(currentResult)
+
+# Convert THB to USD
+thb_to_usd = c.convert('THB', 'USD', 100)
+print('THB to USD : ' + str(thb_to_usd) + "USD")
