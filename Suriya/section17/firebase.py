a713aebbc5f0ecdfe34c82b84806e19a92fb179e
suriyasonp <suriya.sonphu@gmail.com>
2023-05-14 21:05:10 +0700

    add lecture 125 - connecting realtime database

---
+++ Suriya/section17/firebase.py
@@ -0,0 +1,15 @@
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import db
+
+# Fetch the service account key JSON file contents
+cred = credentials.Certificate('mylab-da0bf-firebase-adminsdk-1w6kd-498aefc91d.json')
+
+# Initialize the app with a service account, granting admin privileges
+firebase_admin.initialize_app(cred, {
+    'databaseURL': 'https://mylab-da0bf-default-rtdb.asia-southeast1.firebasedatabase.app/'
+})
+
+# As an admin, the app has access to read and write all data, regradless of Security Rules
+ref = db.reference('restricted_access/secret_document')
+print(ref.get())
