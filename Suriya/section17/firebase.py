bb55ab1ca817c560e196e9b46e7b176b225948e7
suriyasonp <suriya.sonphu@gmail.com>
2023-05-14 21:17:32 +0700

    add read data

--- Suriya/section17/firebase.py
+++ Suriya/section17/firebase.py
@@ -13,3 +13,13 @@ firebase_admin.initialize_app(cred, {
 # As an admin, the app has access to read and write all data, regradless of Security Rules
 ref = db.reference('restricted_access/secret_document')
 print(ref.get())
+
+# Get a database reference to our posts
+user = 'Sommai'
+ref = db.reference('users/' + user + '/age')
+
+# Read the data at the posts reference (this is a blocking operation)
+print('Age of ' + user + ': ' + str(ref.get()))
+
+ref_somsong = db.reference('users/Sommai')
+print(ref_somsong.get())
