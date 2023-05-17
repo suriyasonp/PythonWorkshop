f464c1dad358403a602f54774cb6ca251e8f47af
suriyasonp <suriya.sonphu@gmail.com>
2023-05-17 21:28:30 +0700

    Add current currency rate

--- Suriya/finalProject/CurrentcyConverter.py
+++ Suriya/finalProject/CurrentcyConverter.py
@@ -1,4 +1,5 @@
 from forex_python.converter import CurrencyRates
+import json
 
 
 def print_menu():
@@ -19,5 +20,53 @@ def print_menu():
     return input_menu
 
 
-selected_menu = print_menu()
+def get_currency_rate(source_currency):
+    c = CurrencyRates()
+    rate_result = c.get_rates(source_currency)
+    return rate_result
 
+
+def convert_currency_rate(source_currency, to_currency):
+    pass
+
+
+def access_menu(selected_menu):
+    if selected_menu == '1':
+        print("<" * 10 + "Currency Rate".center(30) + ">" * 10)
+        currency_unit = input(">" * 5 + " Please enter currency unit: >> ")
+        currency_list = get_currency_rate(currency_unit)
+
+        for key, value in currency_list.items():
+            print(">>>  " + (key + " " + str(value).center(30)))
+
+        return question_continue()
+    elif selected_menu == '2':
+        print("Menu 2")
+    elif selected_menu == 'q' or selected_menu == 'Q':
+        pass
+
+
+def question_continue():
+    answer = input("\n\nWould you like to continue? Y=Yes, N=No: >> ")
+    return answer == 'Y' or answer == 'y'
+
+
+def goodbye():
+    print("\n")
+    print("Thank you. See you again.".center(50))
+    print("\n")
+
+
+# Core Program
+
+running = True
+
+while running:
+    selected_menu = print_menu()
+
+    if selected_menu == 'q' or selected_menu == 'Q':
+        running = False
+
+    running = access_menu(selected_menu)
+
+goodbye()
